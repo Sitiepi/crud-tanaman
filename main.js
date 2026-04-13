2d69b41dac1dc3b0b4438bedb395b53408c55459
Sitiepi <sitiepi7@gmail.com>
2026-04-13 10:50:23 +0700

    menambhakan segalanya

--- main.js
+++ main.js
@@ -8,7 +8,9 @@ import {
   deleteDoc,
   updateDoc,
   doc,
-  getDoc
+  getDoc,
+  query,
+  where
 } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js";
 
 // GANTI DENGAN FIREBASE CONFIG ANDA
@@ -24,7 +26,37 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app)
 const tanamanCollection = collection(db, "tanaman")
+  
+  export async function login() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
 
+    const q = query(
+      collection(db,"users"),
+      where("username","==",username),
+      where("password","==", password)
+    )
+  const querySnapshot = await getDocs(collection(db, "users"));
+
+  let found = false;
+
+  querySnapshot.forEach((doc) => {
+    const data = doc.data();
+
+    if (data.username === username && data.password === password) {
+      found = true;
+    }
+  });
+
+  if (found) {
+    document.getElementById("status").innerText = "Login berhasil";
+    // redirect
+    window.location.href = "index.html";
+  } else {
+    document.getElementById("status").innerText = "Username atau password salah";
+  }
+  }
+  
 export async function tampilkanDaftarTanaman() {
   // ambil snapshot data dari koleksi tanaman
   const snapshot = await getDocs(tanamanCollection)
